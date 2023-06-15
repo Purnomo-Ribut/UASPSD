b5b8a1bf5b492c8af5765ef08905f3fe30e85fdc
purnomoribut1148 <purnomoribut1148@gmail.com>
2023-06-15 14:07:56 +0000

    updatepyby pur

--- prediksi.py
+++ prediksi.py
@@ -1,57 +1,163 @@
-#Modul Library
 import streamlit as st
-import numpy as np
 import pandas as pd
-
-#Modul library Metode 
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-
-# #modul library data testing dan training
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
-# #modul library score tingkat akurasi
-from sklearn.metrics import accuracy_score
+st.title('Tugas UAS Proyek Sains Data B ')
+st.text("Isnain Fauziah 200411100007")
+st.text("Nella Adrisia Hartono 200411100107")
+data, preprocessing, modelling, implementasi = st.tabs(["Data","Preprocessing" ,"Modelling", "Implementasi"])
 
-def load_dataset():
-	data = pd.read_csv("BBNI.JK.csv")		
-	return data
 
-st.title('Prediksi Saham PT Bank Rakyat Indonesia (Persero)')
-st.write ("""
-    Kelompok : 
-    * Purnomo Ribut (200411100156)
-    * Dhafa Febriyan Wiranata (200411100169)
-""")
-data, Prepocessing, modelling, implementasi = st.tabs(["Dataset","Prepocessing Data" ,"Modelling", "Implementasi"])
-
+#data
+def read_data():
+    link = "/home/ubuntu/workspace/BBNI.JK.csv"
+    df = pd.read_csv(link, header="infer", index_col=False)
+    return df
+    
+    
 with data:
-    st.write("Tampilan Dataset Saham PT Bank Rakyat Indonesia Persero")
-    st.dataframe(load_dataset())
     st.write("""
-    Dataset Saham PT Bank Rakyat Indonesia Persero yang diambil dari finance.yahoo.com berisi informasi harga saham pada setiap tanggal perdagangan. 
-    Berikut adalah deskripsi dari setiap kolom dalam dataset 
+	Dataset yang digunakan berasal dari data finance yahoo.com dari perusahaan PT Semen Indonesia (Persero) Tbk (SMGR.JK). Data ini berjumlah 247 Data dengan 7 fitur, yaitu: Date, Open, High, Low, Close,Adj, Close, Volume. Data yang digunakan ini merupakan catatan harga dalam kurun waktu 15 Juni 2022 - 15 Juni 2023. Dalam catatan data tersebut dapat dilakukan perhitungan tingkat kesalahan dalam melakukan prediksi beberapa hari yang akan datang .
+	
+	1. Date (Tanggal): Tanggal dalam data time series mengacu pada tanggal tertentu saat data keuangan dikumpulkan atau dilaporkan. Ini adalah waktu kapan data keuangan yang terkait dengan PT Semen Indonesia Indonesia dicatat.
+	2. Open (Harga Pembukaan): Harga pembukaan adalah harga perdagangan PT Adaro Minerals Indonesia pada awal periode waktu tertentu, seperti hari perdagangan atau sesi perdagangan. Harga pembukaan menunjukkan harga perdagangan pertama dari PT Adaro Minerals Indonesia pada periode tersebut.
+	3. High (Harga Tertinggi): Harga tertinggi adalah harga tertinggi yang dicapai oleh PT Adaro Minerals Indonesia selama periode waktu tertentu, seperti hari perdagangan atau sesi perdagangan. Harga tertinggi mencerminkan harga perdagangan tertinggi yang dicapai oleh PT Adaro Minerals Indonesia dalam periode tersebut.
+	4. Low (Harga Terendah): Harga terendah adalah harga terendah yang dicapai oleh PT Adaro Minerals Indonesia selama periode waktu tertentu, seperti hari perdagangan atau sesi perdagangan. Harga terendah mencerminkan harga perdagangan terendah yang dicapai oleh PT Adaro Minerals Indonesia dalam periode tersebut.
+	5. Close (Harga Penutupan): Harga penutupan adalah harga terakhir dari PT Adaro Minerals Indonesia pada akhir periode waktu tertentu, seperti hari perdagangan atau sesi perdagangan. Harga penutupan menunjukkan harga terakhir di mana PT Adaro Minerals Indonesia diperdagangkan pada periode tersebut.
+	6. Adj Close (Harga Penutupan yang Disesuaikan): Adj Close, atau harga penutupan yang disesuaikan, adalah harga penutupan yang telah disesuaikan untuk faktor-faktor seperti dividen, pemecahan saham, atau perubahan lainnya yang mempengaruhi harga saham PT Adaro Minerals Indonesia. Ini memberikan gambaran yang lebih akurat tentang kinerja saham dari waktu ke waktu karena menghilangkan efek dari perubahan-perubahan tersebut.
+	7. Volume: Volume dalam konteks data keuangan PT Adaro Minerals Indonesia mengacu pada jumlah saham yang diperdagangkan selama periode waktu tertentu, seperti hari perdagangan atau sesi perdagangan. Volume mencerminkan seberapa aktifnya perdagangan saham PT Adaro Minerals Indonesia dalam periode tersebut.
+    
+	"""
+	)
+    df=read_data()
+    df
 
-    * Date: Tanggal perdagangan saham.
-    * Open: Harga pembukaan (opening price) saham pada hari perdagangan.
-    * High: Harga tertinggi (highest price) yang dicapai oleh saham pada hari perdagangan.
-    * Low: Harga terendah (lowest price) yang dicapai oleh saham pada hari perdagangan.
-    * Close: Harga penutupan (closing price) saham pada hari perdagangan.
-    * Adj Close: Harga penutupan saham yang telah disesuaikan (adjusted closing price) dengan faktor-faktor seperti pembagian saham, dividen, dan aksi korporasi lainnya.
-    * Volume: Jumlah saham yang diperdagangkan pada hari perdagangan.
+
+with preprocessing:
+		st.write("Data yang telah di preprocessing :")
+		training_set = df.iloc[:197, 1:-1].values
+		test_set = df.iloc[49:, 1:-1].values
+    # Feature Scaling
+		sc = MinMaxScaler(feature_range = (0, 1))
+		training_set_scaled = sc.fit_transform(training_set)
+			# Creating a data structure with 60 time-steps and 1 output
+		X_train = []
+		y_train = []
+		x = 5
+		for i in range(x, 197):
+			X_train.append(training_set_scaled[i-x:i, 0])
+			y_train.append(training_set_scaled[i, 0])
+		X_train, y_train = np.array(X_train), np.array(y_train)
+		X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+		#(740, 60, 1)
+		xtrainbaru = np.reshape(X_train, (192, 5))
+		st.write(xtrainbaru)
+	
 
 
-    Dataset ini dapat digunakan untuk menganalisis pergerakan harga saham PT Bank Rakyat Indonesia serta volume perdagangan yang terjadi dalam rentang waktu tertentu. Dengan menggunakan informasi ini, dapat dilakukan berbagai analisis seperti peramalan harga saham, identifikasi tren pasar, dan pengambilan keputusan investasi.""")
+with  modelling:
+	with st.form("modeling"):
+		st.subheader('Modeling')
+		st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
+		# naive = st.checkbox('Gaussian Naive Bayes')
+		k_nn = st.checkbox('K-Nearest Neighbors')
+		svr = st.checkbox('Support Vector Regression')
+		# destree = st.checkbox('Decision Tree')
+		submitted = st.form_submit_button("Submit")
 
+	if k_nn:
+		# import knn
+		from sklearn.neighbors import KNeighborsRegressor
+		neigh = KNeighborsRegressor(n_neighbors=3)
+		modelknn=neigh.fit(xtrainbaru, y_train)
+		# Definisikan dataset_train dan dataset_test
+		dataset_train = df.iloc[:197, 1:2]  # Menggunakan 45 baris pertama sebagai data latihan
+		dataset_test = df.iloc[49:, 1:2]  # Menggunakan baris setelah 45 sebagai data uji
 
+		# Menggabungkan dataset_train dan dataset_test
+		dataset_total = pd.concat([dataset_train, dataset_test], axis=0)
 
-with Prepocessing : 
-    st.write("Prepocessing dimulai dari : ")
+		# Mengambil input dari dataset_total
+		inputs = dataset_total[len(dataset_total) - len(dataset_test) - 5:].values
+		inputs = inputs.reshape(-1,1)
+		inputs =sc.fit_transform(inputs)
+		X_test = []
+		for i in range(5, 24):
+			X_test.append(inputs[i-5:i, 0])
+		X_test = np.array(X_test)
+		X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+		# print(X_test.shape)
 
-with modelling :
-    st.write("Modelling")
-        
-with implementasi :
-    st.write("Prepocessing dimulai dari : ") 
-        
+		xtestbaru = np.reshape(X_test, (19, 5))
+
+		predicted_pass = modelknn.predict(xtestbaru)
+
+		predicted_pass = predicted_pass.reshape(-1,1)
+		prediksi= sc.inverse_transform(predicted_pass)
+		dataset_test=dataset_test.iloc[0:19]
+    
+		from sklearn.metrics import mean_absolute_percentage_error
+
+		hasil_mape = mean_absolute_percentage_error(dataset_test, prediksi)
+		st.write('MAPE Model K-Nearest Neighbors :', hasil_mape)
+	if svr:
+		from sklearn.svm import SVR
+		svm = SVR(kernel='rbf')
+		model=svm.fit(xtrainbaru, y_train)
+		# import knn
+		# from sklearn.neighbors import KNeighborsRegressor
+		# neigh = KNeighborsRegressor(n_neighbors=3)
+		# modelknn=neigh.fit(xtrainbaru, y_train)
+		# Definisikan dataset_train dan dataset_test
+		dataset_train = df.iloc[:197, 1:2]  # Menggunakan 45 baris pertama sebagai data latihan
+		dataset_test = df.iloc[49:, 1:2]  # Menggunakan baris setelah 45 sebagai data uji
+
+		# Menggabungkan dataset_train dan dataset_test
+		dataset_total = pd.concat([dataset_train, dataset_test], axis=0)
+
+		# Mengambil input dari dataset_total
+		inputs = dataset_total[len(dataset_total) - len(dataset_test) - 5:].values
+		inputs = inputs.reshape(-1,1)
+		inputs =sc.fit_transform(inputs)
+		X_test = []
+		for i in range(5, 24):
+			X_test.append(inputs[i-5:i, 0])
+		X_test = np.array(X_test)
+		X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+		# print(X_test.shape)
+
+		xtestbaru = np.reshape(X_test, (19, 5))
+
+		predicted_pass = model.predict(xtestbaru)
+
+		predicted_pass = predicted_pass.reshape(-1,1)
+		prediksi= sc.inverse_transform(predicted_pass)
+		dataset_test=dataset_test.iloc[0:19]
+    
+		from sklearn.metrics import mean_absolute_percentage_error
+
+		hasil_mape = mean_absolute_percentage_error(dataset_test, prediksi)
+		st.write('MAPE Model Support Vector Regression :', hasil_mape)
+
+	
+
+with  implementasi:
+    with st.form("my_form"):
+        st.write("Implementasi")
+        Open = st.number_input('Open')
+        High = st.number_input('High')
+        Low = st.number_input('Low')
+        Close = st.number_input('Close')
+        Adj_Close = st.number_input('Adj Close')
+        submitted = st.form_submit_button("Submit")
+        a = np.array([[Open,High, Low, Close, Adj_Close]])
+        data_inputan = pd.DataFrame(a, columns= ["Open","High","Low", "Close", "Adj_Close"])
+    if submitted:
+	    st.write(mean_absolute_percentage_error(dataset_test, prediksi))
